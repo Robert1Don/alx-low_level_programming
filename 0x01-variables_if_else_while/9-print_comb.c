c1a7ab5a016233854a58ff77bba7b5c59b35b0b1
Robert1Don <robertedonkor@gmail.com>
2023-03-18 04:48:44 -0700

    combination for successz

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -main function
+ * main - main function
  *
  * Return: always 0
  */
@@ -13,8 +13,12 @@ int main(void)
 	for (i = 0 ; i < 10 ; i++)
 	{
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
+	putchar('\n');
 	return (0);
 }
